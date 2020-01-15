aba44eef289c4ab6d7c4971f5e2a8c95f69a1b7d
Ciro da Silva Gomes <c-iro@hotmail.com>
2020-01-14 23:04:28 -0300

    aula02

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-objyh.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
